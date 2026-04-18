d7196d013f98c0f18adae39836b4b68553b809b6
Binit Acharya <117836161+BinitAcharya7@users.noreply.github.com>
2026-04-18 09:38:59 +0545

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+        const charMap = {}
+        for (let i = 0; i < s.length; i++){
+            charMap[s[i]] = (charMap[s[i]] || 0) + 1
+        }
+        for (let i = 0; i < t.length; i++){
+            if (charMap[t[i]]) charMap[t[i]]--
+            else return false
+        }
+        return true
+    }
+}
